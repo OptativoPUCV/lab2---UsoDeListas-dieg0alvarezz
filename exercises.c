30c12c29fe6ae5c01bcd0a44c91b68727024d1f2
Your Name <you@example.com>
2024-03-24 02:44:16 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -157,11 +157,11 @@ int parentesisBalanceados(char *cadena) {
         ptr++;
     }
 
-    
+    int balanceados = (get_size(pila) == 0); 
     while (get_size(pila) > 0) {
         pop(pila);
     }
     free(pila);
 
-    return 1;
+    return balanceados;
 }
